1083b0cad48b6d1a27ba582696bba02503bbfa06
DavidYusen <yusende@hotmail.com>
2018-08-25 11:07:05 +0800

    Resync on 20180825

--- TradeX.py
+++ TradeX.py
@@ -3,22 +3,25 @@ import logging
 import InsertHKTradeDetails as hk
 import GetDataFromTushare as tu
 import CommonFunctions as cf
+import DrawPictures as dp
 
 if __name__ == '__main__':
     cf.set_log_format()
     logging.info('Main function starts here')
 
     # Refresh basic data
-    # tu.RefreshData()
+    tu.RefreshData()
 
     # Insert HGT trade data by period
-    # hk.insert_data_by_period(20171027, 20171104)
+    hk.insert_data_by_period(20180623, 20180731)
 
     # Insert HGT trade data by date
-    # hk.insert_data_by_date('20171106')
+    # hk.insert_data_by_date('20180326')
 
     # Refresh market data
     tu.refresh_market_data()
+
+    # dp.DrawCandlestick('600690')
 
 
     logging.info('Main function ends here')
